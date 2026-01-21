32da41f7feee3ecaa34c583abb8c550b90d4fe3a
Onur Ülger <ulgeronur@outlook.com>
2026-01-21 23:48:53 +0300

    Add Onur deneme test and ignore config file

--- src/test/java/tests/Onur_DenemeTest.java
+++ src/test/java/tests/Onur_DenemeTest.java
@@ -1,4 +1,11 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class Onur_DenemeTest {
+
+    @Test
+    public void denemeTesti() {
+        System.out.println("Onur branch deneme testi çalıştı");
+    }
 }
