ecb1afcb4b563d878bfee8a2997e05127af59693
HuseyinOzger <huseyinozger1507@gmail.com>
2026-01-25 18:30:14 +0300

    Huseyin//25.01/18.30

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -64,7 +64,7 @@ public class Driver {
             }
 
             driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Long.parseLong(ConfigReader.getProperty("15"))));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Long.parseLong(ConfigReader.getProperty("timeout"))));
         }
 
 
